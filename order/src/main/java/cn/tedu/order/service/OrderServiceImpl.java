e76b1db5961be14b661093a96f5211a2e806a003
980187223@qq.com <Syp950628>
2020-09-09 11:07:18 +0800

    OrderMapper 添加更新订单状态、删除订单&Seata 实现订单的 TCC 操作方法&在业务代码中调用 Try 阶段方法

--- order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
+++ order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
@@ -5,6 +5,8 @@ import cn.tedu.order.feign.AccountClient;
 import cn.tedu.order.feign.EasyIdGeneratorClient;
 import cn.tedu.order.feign.StorageClient;
 import cn.tedu.order.mapper.OrderMapper;
+import cn.tedu.order.tcc.OrderTccAction;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -12,8 +14,8 @@ import java.util.Random;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    @Resource
-    private OrderMapper orderMapper;
+    // @Autowired
+    // private OrderMapper orderMapper;
     @Resource
     EasyIdGeneratorClient easyIdGeneratorClient;
     @Resource
@@ -21,19 +23,32 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private StorageClient storageClient;
 
+    @Resource
+    private OrderTccAction orderTccAction;
+
+    @GlobalTransactional
     @Override
     public void create(Order order) {
         // 从全局唯一id发号器获得id
         Long orderId = easyIdGeneratorClient.nextId("order_business");
         order.setId(orderId);
 
-        orderMapper.create(order);
+        // orderMapper.create(order);
+
+        // 这里修改成调用 TCC 第一节端方法
+        orderTccAction.prepareCreateOrder(
+                null,
+                order.getId(),
+                order.getUserId(),
+                order.getProductId(),
+                order.getCount(),
+                order.getMoney());
 
         // 修改库存
-        storageClient.decrease(order.getProductId(), order.getCount());
+        //storageClient.decrease(order.getProductId(), order.getCount());
 
         // 修改账户余额
-        accountClient.decrease(order.getUserId(), order.getMoney());
+        //accountClient.decrease(order.getUserId(), order.getMoney());
 
     }
 }
